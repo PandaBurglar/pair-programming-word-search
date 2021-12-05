149bda4390e44d0c96c29ff64ef8cad3812effcd
PandaBurglar <zane.bokhari@gmail.com>
2021-12-04 20:31:44 -0500

    look for vertical and horizontal words in a matrix

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,27 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    const verticalMatrix = transpose(letters);
+         
+    const verticalJoin = verticalMatrix.map(vls => vls.join(''))
+    for (vl of verticalJoin) {
+        if (vl.includes(word)) return true
+    }
+ return false;
 }
 
-module.exports = wordSearch
+// IN ORDER TO READ VERTICALLY< TRANPOSE THE MATRIX!!
+const transpose = (matrix) => {
+    const newMatrix = []; // create array 
+    for (const index in matrix[0]) { // for loop for each i in the matrix 
+        const columns = [];
+        for (const arr of matrix) { // declare another array for columns (1-end)
+            columns.push([arr[index]]);
+        }
+        newMatrix.push(columns);;
+        
+    } return newMatrix;
+};
+
+module.exports = wordSearch;
